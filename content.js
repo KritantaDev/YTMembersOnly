b5e33b566eaec1b6b3a0fb4a4d4023b53f2ce3db
KritantaDev <KritantaDevelopment@gmail.com>
2021-03-27 16:05:00 -0500

    Hide donos and dono bar
    Co-authored-by: KritantaDev <KritantaDevelopment@gmail.com>
    Co-authored-by: Alex33856 <8379108+Alex33856@users.noreply.github.com>

--- content.js
+++ content.js
@@ -1,6 +1,13 @@
 var sheets = document.styleSheets;
 var sheet = document.styleSheets[sheets.length - 1];
 
+// hide all messages by default
 sheet.insertRule("yt-live-chat-text-message-renderer {display: none!important;}");
+// override previous and show member messages
 sheet.insertRule("yt-live-chat-text-message-renderer[author-type=\"member\"] {display: inherit!important;}");
+// hide new member messages
 sheet.insertRule("yt-live-chat-membership-item-renderer[show-only-header] {display: none!important;}");
+/* hide donos */
+sheet.insertRule("yt-live-chat-paid-message-renderer {display: none !important;}");
+/* hide dono bar */
+sheet.insertRule("yt-live-chat-ticker-renderer {display: none !important;}");
