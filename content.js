46ba2c4885bff98db66d87f4f591afbbef87f4ba
KritantaDev <KritantaDevelopment@gmail.com>
2021-03-27 16:51:20 -0500

    Rewrite to event based approach

--- content.js
+++ content.js
@@ -1,19 +1,48 @@
 var sheets = document.styleSheets;
 var sheet = document.styleSheets[sheets.length - 1];
 
-// hide all messages by default
 sheet.insertRule("yt-live-chat-text-message-renderer {display: none!important;}");
-// override previous and show member messages
-sheet.insertRule("yt-live-chat-text-message-renderer[author-type=\"member\"] {display: inherit!important;}");
-// hide new member messages
-sheet.insertRule("yt-live-chat-membership-item-renderer[show-only-header] {display: none!important;}");
-/* hide donos */
-sheet.insertRule("yt-live-chat-paid-message-renderer {display: none !important;}");
-/* hide dono bar */
-sheet.insertRule("yt-live-chat-ticker-renderer {display: none !important;}");
-/* remove pfp */
-sheet.insertRule("yt-img-shadow {display: none !important;}");
-/* remove stickers */
-sheet.insertRule("yt-live-chat-paid-sticker-renderer {display: none !important;}");
-/* remove "welcome to live chat" */
-sheet.insertRule("yt-live-chat-viewer-engagement-message-renderer {display: none !important;}");
+sheet.insertRule("yt-live-chat-text-message-renderer.ytsanechat-show {display: inherit!important;}");
+
+const targetNode = document.getElementsByClassName('yt-live-chat-item-list-renderer')[0].children[0].querySelector('#item-offset #items');
+
+// Options for the observer (which mutations to observe)
+const config = { attributes: false, childList: true, subtree: true };
+
+// Callback function to execute when mutations are observed
+const callback = function(mutationsList, observer) 
+{
+    for(const mutation of mutationsList) 
+    {
+        if (mutation.type === 'childList') 
+        {
+            // Messages in spammy streams are rendered in batches
+            for (const item of mutation.addedNodes) 
+            {
+                var isMemberElement = false;
+                if (item.localName == "yt-live-chat-text-message-renderer") 
+                {
+                    if (item.attributes.length >= 3 && item.attributes[2].value == "member")
+                    {
+                        isMemberElement = true;
+                    }
+                    if (!isMemberElement)
+                    {
+                        item.remove();
+                    }
+                    else 
+                    {
+                        item.classList.add("ytsanechat-show");
+                    }
+                }
+
+           }
+        }
+    }
+};
+
+// Create an observer instance linked to the callback function
+const observer = new MutationObserver(callback);
+
+// Start observing the target node for configured mutations
+observer.observe(targetNode, config);
